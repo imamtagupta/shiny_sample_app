d2b5320ec3f6d65d7309a53b5940e7531a3d4de5
imamtagupta <mamtag962000@gmail.com>
2024-02-08 11:51:26 +0530

    update:

--- app/sample.py
+++ app/sample.py
@@ -1,12 +1,39 @@
 from shiny import render, App, ui
+from pathlib import Path
+import matplotlib.pyplot as plt
+import numpy as np
+
+ui.include_css(Path(__file__).parent /"style.css")
 
 app_ui = ui.page_fluid(
-    ui.input_text("in_name", "Enter name", "John"),
-    ui.output_text("display_name")
+    ui.h1("Data visualiser"),
+    ui.row(
+        ui.column(2, ui.h5("Enter Number: ")),
+        ui.column(10,ui.input_numeric("random_num", "","3"))
+    ),
+    # ui.row(
+    #     ui.column(1, ui.h5("Enter URL: ")),
+    #     ui.column(11,ui.input_text_area("url", "","https://restcountries.com/v3.1/all?fields=name,postalCode"))
+    # ),
+    # ui.input_text("in_name", "Enter name", "John"),
+    # ui.output_text("display_name"),
+    ui.output_plot("show_plot"),
 )
 
 def server(input, output, session):
     
+    @output
+    @render.plot
+    def show_plot():
+        np.random.seed(19680801)
+        x_rand = 100 + 15 * np.random.randn(437)
+        fig, ax = plt.subplots()
+        ax.hist(x_rand, int(input.random_num()), density=True)
+        # ui.span(f"number entered is {input.random_num()}")
+        return fig
+
+    
+
     @output
     @render.text
     def display_name():
